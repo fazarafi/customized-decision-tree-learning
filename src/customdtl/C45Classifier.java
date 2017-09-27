0085d6fd124d22ceac2cb3fd9ded5fa6430e0159
Faza Thirafi <faza.thirafi@gmail.com>
2017-09-27 17:27:28 +0700

    add classifier

--- src/customdtl/C45Classifier.java
+++ src/customdtl/C45Classifier.java
@@ -1,5 +1,14 @@
 package customdtl;
 
-public class C45Classifier {
+import weka.classifiers.AbstractClassifier;
+import weka.core.Instances;
+
+public class C45Classifier extends AbstractClassifier {
+
+	@Override
+	public void buildClassifier(Instances input) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/customdtl/MainProgram.java
+++ src/customdtl/MainProgram.java
@@ -1,5 +1,7 @@
 package customdtl;
 
 public class MainProgram {
-
+	public static void main(String[] args) {
+		System.out.print("AAA");
+	}
 }
