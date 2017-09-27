f6c6a368c41ff00e0d46a1732c1bb382f94a4f4c
drestanto <dyas@live.com>
2017-09-27 10:30:41 +0700

    edit gitignore

--- src/customdtl/MainProgram.java
+++ src/customdtl/MainProgram.java
@@ -1,5 +1,10 @@
 package customdtl;
 
 public class MainProgram {
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Dyas");
+    }
 }
