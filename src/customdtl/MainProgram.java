c4e2816d5cd85d9b6f349bc2927cac1c76aeebcb
adesurya16 <adesurya559@gmail.com>
2017-09-27 17:57:03 +0700

    push me

--- src/customdtl/MainProgram.java
+++ src/customdtl/MainProgram.java
@@ -5,6 +5,6 @@ public class MainProgram {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Dyas");
+        System.out.print("Dyas aja");
     }
 }
